6d44e663de6d848bb46664c012c5c9c582945ece
Aaron Howard <arow510@gmail.com>
2017-09-23 14:27:13 -0700

    'getallstates.php'

---
+++ getallstates.php
@@ -0,0 +1,56 @@
+<?php
+
+require('helpers.php');
+
+$statesJson = file_get_contents('states.json');
+
+$states = json_decode($statesJson, true);
+
+
+if (isset($_GET['travel'])) {
+    $travel = $_GET['travel'];
+} else {
+    $travel = '';
+
+}
+
+if (isset($_GET['search'])) {
+  $search = $_GET['search'];
+} else {
+  $search = '';
+}
+
+if (isset($_GET['caseSensitive'])) {
+$caseSensitive = true;
+} else {
+  $caseSensitive = false;
+}
+
+
+
+$hasResults = true;
+if ($search == '') {
+return $states;
+
+}
+
+foreach ($states as $title => $state){
+if($caseSensitive) {
+  $match = $title == $search;
+} else {
+  $match = strtolower($title) == strtolower($search);
+}
+
+if (!$match) {
+    unset($states [$title]);
+  }
+
+}
+
+if (count($states) == 0) {
+
+  $hasResults = false;
+} else {
+
+  $hasResults = true;
+}
---
+++ index.php
@@ -0,0 +1,74 @@
+<?php require('getallstates.php'); ?>
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+<title> </title>
+<meta charset='utf-8'>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css" />
+ <link rel="stylesheet" href= "p2styles.css" />
+
+</head>
+<body>
+
+
+<h1>U.S.A States Abbreviation Lookup</h1>
+
+<form method='GET' action='index.php'>
+
+  <label for='search'>Search:</label>
+  <input type='text' name='search' id='search' value='<?=sanitize($search)?>'>
+ <input type='checkbox' name='caseSensitive' id='caseSensitive' <?php if ($caseSensitive) echo'CHECKED'; ?>>
+  <label for='caseSensitive'>Case Sensitive</label>
+
+
+  <fieldset class='radios'>
+  <legend>Are You Traveling</legend>
+  <input type='radio' name='travel'
+  <?php if (isset($travel) && $travel=='yes') echo "CHECKED";?>
+  value='yes'>Yes
+  <input type='radio' name='travel'
+  <?php if (isset($travel) && $travel=='no') echo 'Checked';?>
+  value='no'>No
+
+  </fieldset>
+
+  <?php if ($_GET) : ?>
+
+                     Traveling: <?=ucfirst($travel)?>
+
+            <?php endif; ?>
+
+
+
+
+
+  <input type='submit' class='btn btn-primary btn-small' value='Filter States'>
+
+
+
+</form>
+
+<?php if(!$hasResults) : ?>
+  <div class='alaert alert-warning'>Your search did not match results.</div>
+<?php elseif ($search != '') : ?>
+<div class='alaert alert-info'>You search for: <?=sanitize($search)?></div>
+
+<?php endif; ?>
+
+<?php foreach ($states as $title => $state) : ?>
+<div class= 'state'>
+<h2>State Name: <?=$state['name']?></h2>
+<h3>Abbreviation: <?=$state['abbreviation']?></h2>
+
+
+</div>
+<?php endforeach; ?>
+
+
+
+</body>
+</html>
